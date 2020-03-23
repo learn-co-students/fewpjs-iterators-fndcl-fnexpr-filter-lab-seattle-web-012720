41de6dcda811b1d6b37f2802f58dc6082446cdda
kailana <kailanakahawaii@gmail.com>
2020-03-22 20:29:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+
+
+
+function findMatching(drivers, string){
+    let findDriver = drivers.filter(driver => {
+        return driver.toUpperCase() === string.toUpperCase()
+    })
+    return findDriver
+}
+
+function fuzzyMatch(drivers, string){
+    let fuzzyDriver = drivers.filter(driver=> {
+        return driver.substr(0, string.length).toUpperCase() === string.toUpperCase()
+    })
+    return fuzzyDriver
+}
+
+function matchName(drivers, string){
+    let matchDriver = drivers.filter(driver => {
+        return driver.name.toUpperCase() === string.toUpperCase()
+    })
+    return matchDriver
+}
